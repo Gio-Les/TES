5db97382e4d9b1e784925bb288d953627f194e10
Giovane <97804009+Gio-Les@users.noreply.github.com>
2023-04-03 11:56:45 -0300

    Add files via upload

---
+++ atividade13.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class atividade13 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in); 
+        int valor;
+        System.out.println("Digite um número: ");
+        valor= ler.nextInt();
+        if (valor >10) {
+            System.out.println("O Número é maior que 10"); 
+         } else { 
+                System.out.println("O Número é menor que 10");
+         }
+            
+        }
+
+    }
+   
+
